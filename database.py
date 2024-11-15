1d0a5fd0caebafa6c7e9a2e407bc8dac29a78672
willcross3 <williamcross24@outlook.com>
2024-11-15 11:16:34 +0000

    Updated database

--- database.py
+++ database.py
@@ -1,2 +1,20 @@
-import sqlite3 as sql
+import sqlite3
 
+connect = sqlite3.connect('inventory.db')
+
+cursor = connect.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS inventory (
+        MAKE TEXT,
+        MODEL TEXT,
+        REG BLOB,
+        MILEAGE INTEGER,
+        YEAR INTEGER,
+        PRICE INTEGER,
+        COLOUR TEXT
+    )
+''');
+
+connect.commit()
+connect.close()
